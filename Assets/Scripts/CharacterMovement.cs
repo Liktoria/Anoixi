53d939038ef19e27a34b91f2e95f264220753efd
Liktoria <69858392+Liktoria@users.noreply.github.com>
2021-03-02 21:18:22 +0100

    Added comments & refactoring

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    //speed the character is moving with
     public float movementSpeed = 1f;
+
+    //the renderer that will display the animation
     CharacterRenderer isoRenderer;
 
     Rigidbody2D rbody;
 
+    //On starting the game (function is called before Start()) get the necessary components from the character game object, the script is attached to
     private void Awake()
     {
         rbody = GetComponent<Rigidbody2D>();
@@ -16,17 +20,26 @@ public class CharacterMovement : MonoBehaviour
     }
 
 
-    // Update is called once per frame
+    // frame-independent function that uses the frequency of the physics system
     void FixedUpdate()
     {
+        //get the current position of the character and the input from 'WASD'
         Vector2 currentPos = rbody.position;
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
+        //save the input into a vector and clamp it to prevent diagonal movement becoming faster than movement in the cardinal directions
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
         inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        
+        //multiply with movement speed and calculate the new position
         Vector2 movement = inputVector * movementSpeed;
-        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime; //use Time.fixedDeltaTime to make sure the movement is stable across different frame rates
+
+        //set direction for the renderer so it can figure out, what animation to play
         isoRenderer.SetDirection(movement);
+
+        //move the character
         rbody.MovePosition(newPos);
         transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
     }
--- Assets/Scripts/Planting.cs
+++ Assets/Scripts/Planting.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 //Handles the click events by the player e.g. for planting flowers. Attach this to the Character game object
-public class ClickHandler : MonoBehaviour
+public class Planting : MonoBehaviour
 {
-    //public GameObject objectToSpawn;
+    //the tilemap where the new tile is supposed to go
     public Tilemap groundTilemap;
+    //the friendly green tile with flowers, Persephone creates by clicking
     public Tile greenTile;
-    public Tile underworldTile;
+    //the position the character is currently placed at in world coordinates
     protected Vector3 characterPosition;
+    //the distance between the clicked spot and the character
     private float distanceToCharacter = 0.0f;
-    public float flowerSpawningFrequency = 0.5F;
-    private float nextSpawn = 0.5F;
-    private float myTime = 0.0F;
 
     // Start is called before the first frame update
     void Start()
@@ -25,36 +24,36 @@ public class ClickHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //When the Input system detects the left mouse button is clicked, flowers are spawned
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Mouse clicked.");
-            nextSpawn = myTime + flowerSpawningFrequency;
             spawnFlowers();
-            nextSpawn = nextSpawn - myTime;
-            myTime = 0.0F;
         }
     }
 
-    //changes the clicked tile to another tile within a certain radius around the character, not pretty yet though
+    //changes the clicked tile to another tile within a certain radius around the character
     void spawnFlowers()
     {
+        //detect the position of the mouse and convert the coordinates to world coordinates
         Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        clickPosition.z = 0;
+        clickPosition.z = 0; //set to 0, because we are working with 2D
+
+        //get the current character position by accessing the transform of the game object this script is attached to
         characterPosition = transform.position;
+
+        //calculate the distance (can maybe be improved to make it more realistic for the player)
         distanceToCharacter = Vector3.Distance(clickPosition, characterPosition);
         Debug.Log("Click distance: " + distanceToCharacter);
 
-        if (distanceToCharacter < 3.8)// && myTime > nextSpawn)
+        //if the distance is small enough (value can be altered to need)
+        if (distanceToCharacter < 3.8)
         {
             Debug.Log("Distance okay.");
+            //determine the cell in the tilemap corresponding to the mouse position
             Vector3Int clickedCell = groundTilemap.WorldToCell(clickPosition);
+            //change the tile to the green tile
             groundTilemap.SetTile(clickedCell, greenTile);
         }
-    }
-
-    //The code to destroy the flowers, when a demon walks over them will go here
-    void destroyFlowers()
-    {
-
     }
 }
